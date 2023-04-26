c7b3228fce0bd11e70de344c4b39d0164ec1c2e0
Caiua Utida <utidapro@gmail.com>
2023-04-26 16:31:19 -0300

    commit with validation and button

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-//alert("Seja Bem Vindo")
+function welcome_alert(){
+    alert("Seja Bem Vindo")
+    alert("Vamos conhecer sobre vinho")
+}
 
 
 function change_color(color1, color2){
     document.body.style.background = "linear-gradient(45deg,"+color1+","+color2+")";
+}
+
+
+function validate(){
+    let user = document.getElementById("user").value;
+    let tel = document.getElementById("tel").value;
+    let email = document.getElementById("email").value;
+
+    if(user == "" || tel == "" || email == ""){
+        alert("Preencha todos os campos")
+    }else{
+        alert("Dados registrados!")
+        window.open('menu.html')
+    }
 }
